b9f399a00e40a609ee92e1c81bd37f01e743faf2
Zachary Ables <zach.ables.ctr@tri.global>
2024-10-11 09:49:30 -0700

    action.yaml

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ python /main.py \
 --file_path "$FILE_PATH" \
 --format_type "$FORMAT_TYPE" \
 --main_key "$MAIN_KEY" \
---sub_key "$SUB_KEY" \
+--sub_keys "$SUB_KEY" \
 --primary_key "$PRIMARY_KEY" \
 --primary_value "$PRIMARY_VALUE" \
 --top_level_keys "$TOP_LEVEL_KEYS"
